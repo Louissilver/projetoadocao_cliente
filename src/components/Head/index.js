71fd48061dee883e24456f46c5eac91bfc4accf0
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-03 20:56:15 -0300

    Tentativa og image 1

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -16,9 +16,6 @@ export default function Home({ title }) {
 
             <meta property="og:description" content="Site de adoção de cachorros." />
             <meta property="og:image" content={carousel} />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="1920" />
-            <meta property="og:image:height" content="1080" />
             <meta property="og:type" content="website" />
 
             <link rel="icon" type="image/jpg" href={icon} />
