05f1fe4238a945567a80c97994634dd13b755886
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-03 21:04:33 -0300

    teste og image 2

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -15,7 +15,7 @@ export default function Home({ title }) {
             <meta property="og:site_name" content="adoCao" />
 
             <meta property="og:description" content="Site de adoção de cachorros." />
-            <meta property="og:image" content={carousel} />
+            <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6TTyQO9pH5fV4r3h9W9RgnAt0KuhtYtdWkg&usqp=CAU" />
             <meta property="og:type" content="website" />
 
             <link rel="icon" type="image/jpg" href={icon} />
