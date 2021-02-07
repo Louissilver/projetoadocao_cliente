48531de27136d019f60a937a10a50fdaacb351ed
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-07 19:39:30 -0300

    ajuste na url de listagem

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,5 @@
 let URL_BACKEND_TOP = 'https://adocao.vercel.app/api'
-if (typeof window !== 'undefined') {
-    window.location.hostname.includes('localhost')
+if (typeof window !== 'undefined' && window.location.hostname.includes('localhost')) {
     URL_BACKEND_TOP = 'http://localhost:3000/api'
 }
 
