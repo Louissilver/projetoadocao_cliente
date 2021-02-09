53e5904c0ae05323435216fb86c65b938bec1c86
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2021-02-09 13:36:28 -0300

    ajuste na url

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-let URL_BACKEND_TOP = 'https://dogapitest.herokuapp.com/'
+let URL_BACKEND_TOP = 'https://dogapitest.herokuapp.com'
 if (typeof window !== 'undefined' && window.location.hostname.includes('localhost')) {
     URL_BACKEND_TOP = 'http://localhost:5000'
 }
